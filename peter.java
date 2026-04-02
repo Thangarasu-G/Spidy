4d17d80da10a90568f83ab5fff2d6a2544425430
Thangarasu-G <thangarasugee@gmail.com>
2026-04-02 10:55:24 +0530

    a 1000 b 2500

---
+++ peter.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class peter 
+{
+public static void main (String[]args)
+{
+  int a,b,c,d;
+  a=1000;
+  b=2500;
+  c=a/b;
+  d=a+b;
+  System.out.println("The Answer Is "+c);
+  System.out.println("The Answer Is "+d);
+
+}
+
+}
