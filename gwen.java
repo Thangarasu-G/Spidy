5aeea158d92eb082bdbe3be78c7d0b740033755e
Thangarasu-G <thangarasugee@gmail.com>
2026-04-02 10:47:22 +0530

    all

--- gwen.java
+++ gwen.java
@@ -6,13 +6,10 @@ public static void main (String[]args)
 {
   int a,b,c;
   a=100;
-<<<<<<< HEAD
   b=250;
   c=a+b;
-=======
   b=25;
   c=a*b;
->>>>>>> af28d7718981a4f86ca671f3af7aec1a83e6670a
   System.out.println("The Answer Is "+c);
 
 }
